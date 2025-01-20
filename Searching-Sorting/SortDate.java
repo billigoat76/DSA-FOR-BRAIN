25bc5dccd0f129079a92dec3610fd9b1c850e20f
VAIBHAV <101047527+billigoat76@users.noreply.github.com>
2025-01-21 00:21:46 +0530

    date sort

--- Searching-Sorting/SortDate.java
+++ Searching-Sorting/SortDate.java
@@ -1,9 +1,9 @@
 public class SortDate {
-    Public static void main(String[] args) {
+    public static void main(String[] args) {
         // Date is in ddmmyyyy format 
-        int[] dates = {12011996, 20111996, 23081996, 30111996, 12081996, 23091996, 30101996};
-        sortDate(nums);
-        for (int num : nums) {
+        int[] dates = {12041996, 20101996, 5061997, 12041989, 11081987};
+        sortDate(dates);
+        for (int num : dates) {
             System.out.print(num + " ");
         }
     }
@@ -11,16 +11,24 @@ public class SortDate {
     public static void sortDate(int[] dates) {
         int date_exp = (int)Math.pow(10, 6);
         
-        countSort(dates,1,31,date_exp);
+        countSort(dates,1,31,date_exp,'d');
         
 
-        int month
-    }
+        int month_exp = (int) Math.pow(10,4);
 
-    public static void countSort(int[] dates,int min,int max,int exp){
+        countSort(dates, 1, 12, month_exp,'m');
+
+        int year_exp = (int) Math.pow(10,0);
+        
+        countSort(dates, 0, 2500, year_exp,'y');
+     }
+
+    public static void countSort(int[] dates,int min,int max,int exp,char dmy){
+        int div = 100;
+        if(dmy=='y') div = (int)Math.pow(10, 4);
         int[] farr = new int[max-min+1];
         for(int i=0;i<dates.length;i++){
-            farr[dates[i]/exp-min]++;
+            farr[dates[i]/exp%div-min]++;
         }
 
         farr[0]--;
@@ -30,9 +38,9 @@ public class SortDate {
 
         int[] ans = new int[dates.length];
         for(int i=dates.length-1;i>=0;i--){
-            int pos = farr[dates[i]/exp];
+            int pos = farr[dates[i]/exp%div-min];
             ans[pos] = dates[i];
-            farr[dates[i]/exp]--;
+            farr[dates[i]/exp%div-min]--;
         }
         for(int i=0;i<ans.length;i++){
             dates[i] = ans[i];
