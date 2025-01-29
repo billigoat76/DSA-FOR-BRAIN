f3bbeed5ee95f9b05b0f95363ce4301849b24c15
VAIBHAV <101047527+billigoat76@users.noreply.github.com>
2025-01-29 13:58:25 +0530

    Leetcode-74,240

---
+++ Searching-Sorting/Leetcode74.java
@@ -0,0 +1,17 @@
+class Leectcode74{ 
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int i = 0 , j = matrix[0].length-1;
+        while(i<matrix.length && j>=0 ){
+            if(target==matrix[i][j]){
+                return true;
+            }
+            else if(target<matrix[i][j]){
+                j--;
+            }
+            else{
+                i++;
+            }
+        }
+        return false;
+    }
+}
